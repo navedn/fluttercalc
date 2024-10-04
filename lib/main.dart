1bad1086c74c7690a2f3cf44de336b4d99c63b56
NavedN <navednoor@yahoo.com>
2024-10-04 11:00:27 -0400

    Added functionality to the number buttons

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Calculator',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Calculator'),
     );
   }
 }
@@ -30,11 +30,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _displayText = '';
 
-  void _incrementCounter() {
+  void _onNumberPressed(String number) {
     setState(() {
-      _counter++;
+      _displayText += number; // Append the number to the display
+    });
+  }
+
+  void _onClearPressed() {
+    setState(() {
+      _displayText = ''; // Clear the display
     });
   }
 
@@ -49,18 +55,15 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
+              _displayText.isEmpty ? '0' : _displayText, // Display text or 0
               style: Theme.of(context).textTheme.headlineMedium,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ElevatedButton(
-                    onPressed: DoNothingAction.new, child: const Text('AC')),
+                    onPressed: _onClearPressed, child: const Text('AC')),
                 ElevatedButton(
                     onPressed: DoNothingAction.new, child: const Text('+/-')),
                 ElevatedButton(
@@ -73,11 +76,14 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ElevatedButton(
-                    onPressed: DoNothingAction.new, child: const Text('7')),
+                    onPressed: () => _onNumberPressed('7'),
+                    child: const Text('7')),
                 ElevatedButton(
-                    onPressed: DoNothingAction.new, child: const Text('8')),
+                    onPressed: () => _onNumberPressed('8'),
+                    child: const Text('8')),
                 ElevatedButton(
-                    onPressed: DoNothingAction.new, child: const Text('9')),
+                    onPressed: () => _onNumberPressed('9'),
+                    child: const Text('9')),
                 ElevatedButton(
                     onPressed: DoNothingAction.new, child: const Text('*')),
               ],
@@ -86,11 +92,14 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ElevatedButton(
-                    onPressed: DoNothingAction.new, child: const Text('4')),
+                    onPressed: () => _onNumberPressed('4'),
+                    child: const Text('4')),
                 ElevatedButton(
-                    onPressed: DoNothingAction.new, child: const Text('5')),
+                    onPressed: () => _onNumberPressed('5'),
+                    child: const Text('5')),
                 ElevatedButton(
-                    onPressed: DoNothingAction.new, child: const Text('6')),
+                    onPressed: () => _onNumberPressed('6'),
+                    child: const Text('6')),
                 ElevatedButton(
                     onPressed: DoNothingAction.new, child: const Text('-')),
               ],
@@ -99,11 +108,14 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ElevatedButton(
-                    onPressed: DoNothingAction.new, child: const Text('1')),
+                    onPressed: () => _onNumberPressed('1'),
+                    child: const Text('1')),
                 ElevatedButton(
-                    onPressed: DoNothingAction.new, child: const Text('2')),
+                    onPressed: () => _onNumberPressed('2'),
+                    child: const Text('2')),
                 ElevatedButton(
-                    onPressed: DoNothingAction.new, child: const Text('3')),
+                    onPressed: () => _onNumberPressed('3'),
+                    child: const Text('3')),
                 ElevatedButton(
                     onPressed: DoNothingAction.new, child: const Text('+')),
               ],
@@ -112,20 +124,17 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ElevatedButton(
-                    onPressed: DoNothingAction.new, child: const Text('0')),
+                    onPressed: () => _onNumberPressed('0'),
+                    child: const Text('0')),
                 ElevatedButton(
-                    onPressed: DoNothingAction.new, child: const Text('.')),
+                    onPressed: () => _onNumberPressed('.'),
+                    child: const Text('.')),
                 ElevatedButton(
                     onPressed: DoNothingAction.new, child: const Text('=')),
               ],
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
